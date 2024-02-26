9eb3933d092ba73eea76b770f913b08c61cfd6f9
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-02-26 20:39:32 -0300

    Comit inicial

---
+++ src/test/java/com/pauloalecio/springemailservice/SpringEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloalecio.springemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
